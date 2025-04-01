2eb034f5262f7aed1f68e9127486e063a97ac07b
Sumith Sigtia <137398119+Sumithsigtia@users.noreply.github.com>
2025-04-01 05:30:57 +0530

    Update HotelDetails.tsx

--- src/app/output/demo/HotelDetails.tsx
+++ src/app/output/demo/HotelDetails.tsx
@@ -15,7 +15,7 @@ interface HotelData {
 
 const HotelDetails: React.FC<HotelDetailsProps> = ({ location, checkinDate, checkoutDate }) => {
   const [hotelData, setHotelData] = useState<HotelData[]>([]);
-  const baseUrl = 'https://hudson-colorado-relatives-constantly.trycloudflare.com';
+  const baseUrl = 'https://international-tables-management-pattern.trycloudflare.com';
 
   const fetchHotelDetails = async () => {
     try {
